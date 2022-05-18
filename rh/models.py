7b41381c82685b1ad4cf219e1f7664ab8fcea9ad
MateusMessias <88012579+MateusMessias@users.noreply.github.com>
2022-05-18 01:55:01 -0300

    Testes 1

--- rh/models.py
+++ rh/models.py
@@ -14,14 +14,14 @@ class Area (models.Model):
     Empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
-        return f'{self.nome} - Dep: {self.departamento.nome}'
+        return f'{self.nome} - Dep: {self.Departamento.nome} - Empresa: {self.Empresa.nome} '
 
 
 class Colaboradores (models.Model):
-	Nome = models.CharField(max_length=255)
-	Salario= models.DecimalField(decimal_places=2, max_digits=10)
-	Horas_Extra = models.DecimalField(decimal_places=1, max_digits=4)
-Cargo = models.ForeignKey(Area, on_delete=models.PROTECT)
+    Nome = models.CharField(max_length=255)
+    Salario= models.DecimalField(decimal_places=2, max_digits=10)
+    Horas_Extra = models.DecimalField(decimal_places=1, max_digits=4)
+    Cargo = models.ForeignKey(Area, on_delete=models.DO_NOTHING)
 
 def __str__(self):
         return self.nome
@@ -36,22 +36,22 @@ class Ponto (models.Model):
         return f'Colab {self.Colaborador} - {self.Entrada} :: {self.Saida} / {self.Carga_Horaria}'
 
 class Notas (models.Model):
-	Numero = models.DecimalField(decimal_places=2, max_digits=100)
-Data = models.DateField()
-Fornecedor = models.ForeignKey(Empresa, on_delete=models.CASCADE)
-Cliente = models.CharField(max_length=255)
-Imposto = models.DecimalField(decimal_places=2, max_digits=10)
-Valor_Nota = models.DecimalField(decimal_places=2, max_digits=100)
+    Numero = models.DecimalField(decimal_places=2, max_digits=100)
+    Data = models.DateField()
+    Fornecedor = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    Cliente = models.CharField(max_length=255)
+    Imposto = models.DecimalField(decimal_places=2, max_digits=10)
+    Valor_Nota = models.DecimalField(decimal_places=2, max_digits=100)
 
 def __str__(self):
         return f'Nota {self.Numero} : {self.Data} : {self.Valor_Nota}'
 
 class Contabilidade (models.Model):
     Nome = models.ForeignKey(Empresa, on_delete=models.PROTECT)
-Saldo = models.DecimalField(decimal_places=2, max_digits=100)
-Despesas = models.DecimalField(decimal_places=2, max_digits=100)
-Salarios = models.DecimalField(decimal_places=2, max_digits=100)
-Arrecadação = models.DecimalField(decimal_places=2, max_digits=100)
+    Saldo = models.DecimalField(decimal_places=2, max_digits=100)
+    Despesas = models.DecimalField(decimal_places=2, max_digits=100)
+    Salarios = models.DecimalField(decimal_places=2, max_digits=100)
+    Arrecadação = models.DecimalField(decimal_places=2, max_digits=100)
 
 def __str__(self):
         return self.Nome
