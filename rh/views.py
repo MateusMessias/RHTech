c5803922c86b75a783f94bce0dfccf899bc38107
MateusMessias <88012579+MateusMessias@users.noreply.github.com>
2022-07-06 00:43:30 -0300

    Atulização Aula NoSQL

--- rh/views.py
+++ rh/views.py
@@ -1,4 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
+import pprint
+from pymongo import MongoClient
 
 def index(request):
     return HttpResponse("Bem indo ao RHTech.")
+
+def index(request):
+    client = MongoClient('mongodb+srv://mongo:mongo@aulasenai.kwftl.mongodb.net/?retryWrites=true&w=majority')
+    
+    db = client.rhtech
+    print('db', db)
+    print(db.list_collection_names())
+    
+    pontos = db.ponto
+    
+    pprint.pprint(pontos.find_one())
