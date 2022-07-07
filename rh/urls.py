a9a152cdf80b67ef63b317bdf73fd98ad6562a32
MateusMessias <88012579+MateusMessias@users.noreply.github.com>
2022-07-07 00:27:24 -0300

    Conexão Redis

--- rh/urls.py
+++ rh/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registro_mongo', views.registro_mongo, name='registro_mongo'),
+    path('ponto_redis', views.ponto_redis),
+    
 ]
 
--- rh/views.py
+++ rh/views.py
@@ -1,19 +1,87 @@
+from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 import pprint
 from pymongo import MongoClient
 
-def index(request):
-    return HttpResponse("Bem indo ao RHTech.")
+import redis
+
 
 def index(request):
-    client = MongoClient('mongodb+srv://mongo:mongo@aulasenai.kwftl.mongodb.net/?retryWrites=true&w=majority')
+    return render(request, 'index.html')
+
+def registro_mongo(request):
+        
+    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+
+    if is_ajax:
+        try:
+            ponto = {
+                "colaborador" : {
+                    "id" : 25.0, 
+                    "nome" : "Tiago Lopes", 
+                    "idade" : 32.0, 
+                    "cargo" : {
+                        "id" : 33.0, 
+                        "cargo" : "Analista", 
+                        "departamento" : "Suporte Tecnico"
+                    }
+                }, 
+                "data" : "2022-06-02", 
+                "ponto" : [
+                    {
+                        "tipo" : "entrada", 
+                        "hora" : "08:10:49.689"
+                    }, 
+                    {
+                        "tipo" : "saida", 
+                        "hora" : "12:15:49.689"
+                    }, 
+
+                ], 
+                "jornada" : 8.75  
+            }
+  
+            client = MongoClient('mongodb+srv://mongo:mongo@aulasenai.kwftl.mongodb.net/?retryWrites=true&w=majority')
     
-    db = client.rhtech
-    print('db', db)
-    print(db.list_collection_names())
+            db = client.rhtech
+            print('db', db)
+            print(db.list_collection_names())
+
+            pontos = db.folha_ponto
+            ponto_id = pontos.insert_one(ponto).inserted_id
+            ponto_id
+
+        except:
+            return JsonResponse({'response': 'Invalid request'}, status=400)
+        
+    else:
+        return JsonResponse({'response': 'Invalid request'}, status=400)
     
-    pontos = db.ponto
+    return JsonResponse({'response': f'Ponto {ponto_id} registrado com sucesso'}, )
+
+
+def ponto_redis(request):
+    print(request)
     
-    pprint.pprint(pontos.find_one())
+    try:    
+        r = redis.Redis(
+            host='redis-10782.c10.us-east-1-2.ec2.cloud.redislabs.com:10782',
+            port=17597, 
+            password='Tx8rR5v7fguToeN7rowBSeit5IpppTrd')
+        
+        print('Registro Ponto', r)
+
+        chave = f'Mateus-{datetime.now().strftime("%d%m%Y%S")}'
+        print(chave)
+        r.set(chave, str(datetime.now()))
+        
+        value = r.get(chave)
+        print(value)
+            
+    except:
+        return JsonResponse({'response': 'Invalid request'}, status=400)
+        
+    
+    return JsonResponse({'response': f'ponto {chave} : {value}'}, )
